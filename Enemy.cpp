09a78306c4207e2e49a94be859abe4af2570b23a
AJderpscout <119960512+AJderpscout@users.noreply.github.com>
2024-11-26 13:41:50 -0800

    Test to see if people can see my files

---
+++ Enemy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include "Enemy.h"
+using namespace std;
+
+int Enemy::enemyTakeDamage(){
+    
+
+}
+void Enemy::enemyAttackToHit(){
+    srand()
+
+}
---
+++ Enemy.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+class Enemy{
+    private:
+        int enemyHealthValue;
+        int enemyAttackValue;
+    public:
+        int enemyAttackToHit()
+        void enemyTakeDamage()
+
+
+
+};
